448a5e6fb35123ce317cd9b29c14796b048b8ff7
nghia21-1 <nghialequang21@gmail.com>
2023-04-11 22:09:54 +0700

    first commit

---
+++ ecommerce-app/src/utils/Data.js
@@ -0,0 +1,32 @@
+const services = [
+
+    {
+        title: "Free Shipping",
+        tagline: "From all orders over $5",
+        image: "image/service.png",
+    },
+
+    {
+        title: "Daily Suprise Offers",
+        tagline: "Save off 25%",
+        image: "image/service-02.png",
+    },
+
+    {
+        title: "Support 24/7",
+        tagline: "Shop with an expert",
+        image: "image/service-03.png",
+    },
+
+    {
+        title: "Affordable Pries",
+        tagline: "Get Factory Default Price",
+        image: "image/service-04.png",
+    },
+
+    {
+        title: "Secure Payments",
+        tagline: "100% Protected Payment",
+        image: "image/service-05.png",
+    },
+];
